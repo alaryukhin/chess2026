5597c0a9dd1d77571fc5f782fade96e3b5351e10
alaryukhin <anya.laryukhin@gmail.com>
2026-02-25 15:55:35 +0000

    Upgrade Java source/target to 21

--- src/main/java/com/example/Board.java
+++ src/main/java/com/example/Board.java
@@ -74,8 +74,16 @@ public class Board extends JPanel implements MouseListener, MouseMotionListener 
         //the reason this is required has to do with how visual components are rendered, so if you neglect to do this
         //you will not see any of your squares show up on the board!
         // Where's the "add" method? Stay tuned for next unit where we discover where it is and why we can do this action.
-        
-        
+        boolean isWhite = true;
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                Square sq = new Square(isWhite, row, col);
+                board[row][col] = sq;
+                this.add(sq);
+                isWhite = !isWhite;
+            }
+            isWhite = !isWhite;
+        }
         initializePieces();
 
         this.setPreferredSize(new Dimension(400, 400));
